e6220ac6b090f1d50b4fd684068bf6e524ab4ce1
NafisAhnnaf <126236370+NafisAhnnaf@users.noreply.github.com>
2025-02-10 00:41:13 +0600

    Delete frontend/ReactFornt directory

--- frontend/ReactFornt/src/Components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Button = ({name, type, func}) => {
-  return (
-    <button onClick={func} className={`${type} rounded-xl px-3 py-2`}>{name}</button>
-  )
-}
-
-export default Button
